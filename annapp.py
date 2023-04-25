b4172ea1b31920e4b3bf31ef04456a8ad9be59fa
sentiment-analysi <129092803+sentiment-analysi@users.noreply.github.com>
2023-04-25 14:53:54 +0530

    Update annapp.py

--- annapp.py
+++ annapp.py
@@ -20,47 +20,41 @@ cv = pickle.load(open('count-Vectorizer.pkl','rb'))
 sc = pickle.load(open('Standard-Scaler.pkl','rb'))
 
 # Function to perform sentiment analysis
-def predict_sentiment(input_review):
-    input_review = re.sub(pattern='[^a-zA-Z]', repl=' ', string=input_review)
-    input_review = input_review.lower()
-    input_review_words = input_review.split()
+def predict_sentiment(df, column_name):
+    input_reviews = df[column_name].tolist()
+    input_reviews = [re.sub(pattern='[^a-zA-Z]', repl=' ', string=review) for review in input_reviews]
+    input_reviews = [review.lower() for review in input_reviews]
+    input_reviews = [review.split() for review in input_reviews]
     stop_words = set(stopwords.words('english'))
     stop_words.remove('not')
-    input_review_words = [word for word in input_review_words if not word in stop_words]
+    input_reviews = [[word for word in review_words if not word in stop_words] for review_words in input_reviews]
     ps = PorterStemmer()
-    input_review = [ps.stem(word) for word in input_review_words]
-    input_review = ' '.join(input_review)
-    input_X = cv.transform([input_review]).toarray()
+    input_reviews = [[ps.stem(word) for word in review_words] for review_words in input_reviews]
+    input_reviews = [' '.join(review_words) for review_words in input_reviews]
+    input_X = cv.transform(input_reviews).toarray()
     input_X = sc.transform(input_X)
     pred = classifier.predict(input_X)
     pred = (pred > 0.5)
-    if pred[0][0]:
-        return "Positive review"
-    else:
-        return "Negative review"
+    sentiment = ['Positive review' if p else 'Negative review' for p in pred]
+    return sentiment
+
 
 
 # Function to show the analytics in a separate tab
-def show_analytics(df):
-    # List of columns to perform sentiment analysis on
-    columns = ['question1', 'question2']
-
-    # Concatenate the results of sentiment analysis into a single column
-    df['Sentiment'] = ''
-    for col in columns:
-        df[col] = df[col].fillna('')
-        df['Sentiment'] += df[col].apply(predict_sentiment) + ' '
-
+def show_analytics(df, column_name):
+    # Apply sentiment analysis to specified column
+    sentiments = predict_sentiment(df, column_name)
+    
     # Get the count of reviews and positive/negative reviews
-    total_reviews = len(df)
-    positive_reviews = len(df[df['Sentiment'].str.contains('Positive review')])
-    negative_reviews = len(df[df['Sentiment'].str.contains('Negative review')])
+    total_reviews = len(sentiments)
+    positive_reviews = sentiments.count('Positive review')
+    negative_reviews = sentiments.count('Negative review')
     
     # Print the count of reviews and positive/negative reviews
     st.write(f"Total number of reviews: {total_reviews}")
     st.write(f"Number of positive reviews: {positive_reviews}")
     st.write(f"Number of negative reviews: {negative_reviews}")
-
+    
     # Plot the sentiment analysis results using matplotlib
     fig, ax = plt.subplots()
     ax.bar(['Positive', 'Negative'], [positive_reviews, negative_reviews], color=['blue', 'orange'])
@@ -71,21 +65,22 @@ def show_analytics(df):
 
 
 
+
 # Main function to run the app
 def main():
     st.title('Student sentiment analysis')
 
     # Get the user inputs
     file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
-
-    # Perform sentiment analysis and show the results
     if file is not None:
         df = pd.read_excel(file)
+        column_name = st.selectbox('Select column to analyze:', df.columns)
         st.write(df)
         
         # Show analytics in a separate tab on click of a button
         if st.button('Show Analytics'):
-            show_analytics(df)
+            show_analytics(df, column_name)
+
 
 # Run the app
 if __name__=='__main__':
